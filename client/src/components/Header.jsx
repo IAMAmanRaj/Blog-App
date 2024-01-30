bcf726f59b92de39b08eba69dae53c20c1971450
IAMAmanRaj <imamanraj87@gmail.com>
2024-01-30 08:25:44 +0530

    change header in auth app

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const path = useLocation().pathname;
+
+  return (
+    <Navbar className="border-b-2 ">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Aman's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="">
+          <FaMoon />
+        </Button>
+        <Link to="/profile">
+          {currentUser ? (
+            <img
+              src={currentUser.profilePicture}
+              alt="profile"
+              className="'h-7 w-7 rounded-full object-cover"
+            />
+          ) : (
+            <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          )}
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
