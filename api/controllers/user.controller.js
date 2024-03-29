de1fe5ca70413ea4532ba5bb9ee3c642e32c5a22
IAMAmanRaj <imamanraj87@gmail.com>
2024-03-30 00:18:43 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -40,7 +40,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id != req.params.id) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(401, "You can delete only your account"));
   }
   try {
