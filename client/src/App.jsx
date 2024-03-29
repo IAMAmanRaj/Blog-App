d839a93eb3229899d4bc8244667cfeaf51d9ef5e
IAMAmanRaj <imamanraj87@gmail.com>
2024-03-30 02:26:24 +0530

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
 
           <Route path="/projects" element={<Projects />} />
+          <Route path='/post/:postSlug' element={<PostPage />} />
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
