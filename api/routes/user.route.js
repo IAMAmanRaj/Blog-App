df7149765f52cbc5d19aa603b70caffe2bf4ec4a
IAMAmanRaj <imamanraj87@gmail.com>
2024-03-29 23:19:08 +0530

    update profile section

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,7 +9,7 @@ import {
 const router = express.Router();
 
 router.get("/", test);
-router.post("/update/:id", verifyToken, updateUser);
+router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
